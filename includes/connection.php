511a1a69c984c0ff742cf77003ee42d04f973451
kmike-wachira <mwkariuki2e@gmail.com>
2020-02-10 12:21:35 +0300

    added sessions and backend to edit form

--- includes/connection.php
+++ includes/connection.php
@@ -1,20 +1,24 @@
+
 <?php
+session_start();
 $conn = new mysqli('localhost', 'root', '', 'masenocu_db');
-// Check connection
 
-if ($conn->connect_error) {
+
+  if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}
+  }
 
 
-if(isset($_POST['activity'])){
+  if(isset($_POST['activity'])){
   $actiTitle=$_POST['activityTitle'];
   $actiDate=$_POST['activityDate'];
   $actiCategory=$_POST['activityCategory'];
   $actiVenue=$_POST['activityVenue'];
   $actiSummary=$_POST['activitySummary'];
+  $actiTime=$_POST['activityTime'];
+
   $sql = "INSERT INTO `activities`(`title`, `description`, `date`, `venue`, `category`)
-   VALUES ('$actiTitle','$actiSummary','$actiDate','$actiVenue','$actiCategory')";
+   VALUES ('$actiTitle','$actiSummary','$actiDate $actiTime','$actiVenue','$actiCategory')";
 
   if ($conn->query($sql) === TRUE) {
     // echo "added succesfully";
@@ -27,7 +31,7 @@ if(isset($_POST['activity'])){
 // sermon php
 
 
-if(isset($_POST['sermon'])){
+  if(isset($_POST['sermon'])){
   $sermonTitle=$_POST['sermonTitle'];
   $sermonDate=$_POST['sermonDate'];
   $sermonAudio=$_POST['sermonAudio'];
@@ -37,9 +41,9 @@ if(isset($_POST['sermon'])){
   $sermonTime=$_POST['sermonTime'];
   $sermonSpeaker=$_POST['sermonSpeaker'];
   $sermonSummary=$_POST['sermonSummary'];
-    $date=$sermonDate. $sermonTime;
+    // $date=$sermonDate . $sermonTime;
   $sermonsql = "INSERT INTO `sermons`(`title`, `summary`, `date`, `video`, `audio`, `document`, `venue`, `speaker`)
- VALUES ('$sermonTitle','$sermonSummary','$date','$sermonVideo','$sermonAudio','$sermonDocument','$sermonVenue','$sermonSpeaker')";
+  VALUES ('$sermonTitle','$sermonSummary','$sermonDate $sermonTime','$sermonVideo','$sermonAudio','$sermonDocument','$sermonVenue','$sermonSpeaker')";
 
   if ($conn->query($sermonsql) === TRUE) {
     // echo "added succesfully";
@@ -51,22 +55,22 @@ if(isset($_POST['sermon'])){
 }
 
 // Leadership
-if(isset($_POST['Leaders'])){
+  if(isset($_POST['Leaders'])){
     $leaderGid=$_POST['leaderGid'];
     $leaderMId=(int)$_POST['leaderMId'];
     $leaderDocket=$_POST['leaderDocket'];
-  $leadersql = "INSERT INTO `leaders`( `docket`, `member_id`, `group_id`)
-  VALUES ('$leaderDocket',$leaderMId,$leaderGid)";
+    $leadersql = "INSERT INTO `leaders`( `docket`, `member_id`, `group_id`)
+    VALUES ('$leaderDocket',$leaderMId,$leaderGid)";
 
-  if ($conn->query($leadersql) === TRUE) {
-      header('location:../superAdmin.php');
-  } else {
-      echo "Error: " . $leadersql . "<br>" . $conn->error;
-  }
+    if ($conn->query($leadersql) === TRUE) {
+        header('location:../superAdmin.php');
+    } else {
+        echo "Error: " . $leadersql . "<br>" . $conn->error;
+    }
   // $conn->close();
-}
+  }
 
-    function testimonial($idn){
+  function testimonial($idn){
       $conne = new mysqli('localhost', 'root', '', 'masenocu_db');
       // Check connection
 
@@ -97,7 +101,7 @@ if(isset($_POST['Leaders'])){
 
        // semester theme
 
-   if(isset($_POST['theme'])){
+  if(isset($_POST['theme'])){
      $theme=$_POST['semTheme'];
      $verse=$_POST['verse'];
      $semester=$_POST['semester'];
@@ -113,5 +117,70 @@ if(isset($_POST['Leaders'])){
      }
      // $conn->close();
    }
+  if(isset($_POST['signup'])){
+    $memberfirstname=$_POST['memberfirstname'];
+    $memberlastname=$_POST['memberlastname'];
+    $memberemail=$_POST['memberemail'];
+    $memberyos=$_POST['memberyos'];
+    $memberphone=$_POST['memberphone'];
+    $memberpassword=$_POST['memberpassword'];
+    $memberpassword=$_POST['memberpassword'];
+    $memberCpassword=$_POST['memberCpassword'];
+    if($memberpassword == $memberCpassword){
+      $membersql="INSERT INTO `members`(`first_name`, `last_name`, `email`, `phone`, `study_year`,`password`)
+       VALUES ('$memberfirstname','$memberlastname','$memberemail','$memberphone','$memberyos','$memberpassword')";
+      if ($conn->query($membersql) === TRUE) {
+        // echo "added succesfully";
+          header('location:../login.php');
+      } else {
+          header('location:../sign-up.php');
+          echo "Error: " . $membersql . "<br>" . $conn->error;
+      }
+    }else{
+      header('location:../sign-up.php');
+      echo "Password mismatch" ;
+    }
+  }
 
+  if (isset($_POST['login'])) {
+     echo "0 results";
+    $loginemail = $_POST['loginemail'];
+    $loginpassword =  $_POST['loginpassword'];
+    $loginquery = "SELECT * FROM `members` WHERE `email` = '$loginemail' AND `password` = '$loginpassword'";
+    $login_result = $conn->query($loginquery);
+    if ($login_result->num_rows > 0) {
+     $_SESSION['user']=$loginemail;
+
+        header('location:../index.php');
+    }else {
+       // echo "0 results";
+    }
+
+
+  }
+
+
+  // edit profile
+  if (isset($_POST['edit'])) {
+  $email=$_SESSION['user'];
+   $image = $_FILES['image']['name'];
+   $course = mysqli_real_escape_string($conn,$_POST['course']);
+   $target = "../images/".basename($image);
+   $imagename=basename($image);
+   $sql = "UPDATE `members` SET `picture`='$imagename' , `course`='$course' WHERE `email`='$email'";
+
+   if ($conn->query($sql) === TRUE) {
+     header('location:../profile.php');
+     // echo "<script type='text/javascript'>alert('Record updated successfully');</script>";
+   } else {
+    echo "Error updating record: " . $conn->error;
+    }
+   if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+   $msg = "Image uploaded successfully";
+   echo "<script type='text/javascript'>alert('Image uploaded successfully');</script>";
+
+   }else{
+     echo "<script type='text/javascript'>alert('Image failed upload');</script>";
+   }
+ }
  ?>
