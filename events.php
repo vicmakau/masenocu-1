1020cba2b957c08577889bff2de78198052cd03e
kmike-wachira <mwkariuki2e@gmail.com>
2020-02-10 12:21:03 +0300

    added backed for dynamicity

--- events.php
+++ events.php
@@ -21,48 +21,71 @@ include("includes/header.php");
 </section>
 <!-- END section -->
 
-<section class="site-section bg-light">
-  <div class="container">
-    <div class="row">
-      <?php
-      // $datainClient = new FetchNews();
-      // $datainClient->fetchNewsfuction(3);
-      ?>
-    </div>
-
-    <div class="row mt-5">
-      <div class="col-md-12 pt-5">
-        <ul class="pagination custom-pagination">
-          <li class="page-item prev"><a class="page-link" href="#"><i class="ion-ios-arrow-back"></i></a></li>
-          <li class="page-item active"><a class="page-linkx href=" #">1</a></li>
-          <li class="page-item"><a class="page-link" href="#">2</a></li>
-          <li class="page-item"><a class="page-link" href="#">3</a></li>
-          <li class="page-item next"><a class="page-link" href="#"><i class="ion-ios-arrow-forward"></i></a></li>
-        </ul>
 
 
-      </div>
-    </div>
-  </div>
-</section>
+      <section class="site-section bg-light">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="section-heading">
+                <h2 class="heading">Church Ministry</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="block-13">
+          <div class="nonloop-block-13 owl-carousel">
+            <?php
+            $event_sql = "SELECT * FROM `activities` ORDER BY date DESC";
+            $event_result = mysqli_query($db, $event_sql);
+            if (mysqli_num_rows($event_result) > 0) :
+              while($event_row = mysqli_fetch_assoc($event_result)):?>
+                              <div class="item">
+                                <div class="block-20">
+                                  <figure>
+                                    <a href="#"><img src="https://z-p3-scontent.fmba2-1.fna.fbcdn.net/v/t1.0-0/p200x200/49671095_590680258043788_8824584078608039936_n.jpg?_nc_cat=111&_nc_ohc=rDCKJhB-e08AQm5pBk3d5EoDPnrbgKc7xB_sUZfsnWcO4COvL3WJniAig&_nc_ht=z-p3-scontent.fmba2-1.fna&oh=cd93cd292d7da10cb2a7d3f8bc0c4770&oe=5E4E9106" alt="Maseno Cu" height"50" class="img-fluid"></a>
+                                  </figure>
+                                  <div class="text text-center">
+                                    <h4 class="heading"><a href="#"><?=$event_row['title']?></a></h4>
+                                    <p>Date <?php
+                                    $d=strtotime($event_row['date']);
+                                    echo date("d M ,Y ", $d);echo "Time ".date("H:i ", $d);
+                                      ?>:<br>Venue <b> <?= $event_row['venue']?></b> </p>
+                                    <p class="event-description"> <?=$event_row['description'] ?> </p>
+                                    <!-- <p><a href="#">Read More</a></p> -->
+                                  </div>
+                                </div>
+                              </div>
+              <?php
+            endwhile;
+            endif;
+            ?>
+          </div>
+        </div>
+      </section>
+
+
 <?php include("includes/footer.php"); ?>
-</div>
+
 
 <!-- loader -->
 <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
-    <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+    <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/>
     <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#f4b214" /></svg></div>
 
-<script src="js/jquery-3.2.1.min.js"></script>
-<script src="js/jquery-migrate-3.0.0.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/owl.carousel.min.js"></script>
-<script src="js/jquery.waypoints.min.js"></script>
-<script src="js/jquery.stellar.min.js"></script>
-<script src="js/jquery.animateNumber.min.js"></script>
-<script src="js/jquery.magnific-popup.min.js"></script>
-<script src="js/main.js"></script>
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/jquery-migrate-3.0.0.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+
+    <script src="js/main.js"></script>
 </body>
 
 </html>
